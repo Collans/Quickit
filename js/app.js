1db9f140eac5b77ea4a84d89dbbea46d5991557c
Phorell <96635163+Collans@users.noreply.github.com>
2023-09-10 12:48:24 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,29 +14,6 @@
             document.documentElement.classList.add(className);
         }));
     }
-    let isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        any: function() {
-            return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
-        }
-    };
-    function addTouchClass() {
-        if (isMobile.any()) document.documentElement.classList.add("touch");
-    }
     function addLoadedClass() {
         if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
             setTimeout((function() {
@@ -572,9 +549,8 @@
     }
     const da = new DynamicAdapt("max");
     da.init();
-    window["FLS"] = true;
+    window["FLS"] = false;
     isWebp();
-    addTouchClass();
     addLoadedClass();
     menuInit();
     tabs();
